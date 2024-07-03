828d8a13bff789bc27e7d785d71eedee0209c6a6
Cindy Andrade <cindya3939@gmail.com>
2024-07-03 14:57:21 -0700

    Working checkboxes and image setting

--- src/BonusGame.js
+++ src/BonusGame.js
@@ -5,11 +5,29 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 export default function BonusGame({score, setScore}){
+    let imageHeightValue = "175px";
+
     const[id, setID] = useState("");
     const[ingredient, setIngredient] = useState("");
     const[recipeImage1, setRecipeImage1] = useState("https://images.unsplash.com/photo-1533745848184-3db07256e163?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
     const[recipeImage2, setRecipeImage2] = useState("https://images.unsplash.com/photo-1533745848184-3db07256e163?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
     const[recipeImage3, setRecipeImage3] = useState("https://images.unsplash.com/photo-1533745848184-3db07256e163?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+    const[recipeTitle1, setRecipeTitle1] = useState("");
+    const[recipeTitle2, setRecipeTitle2] = useState("");
+    const[recipeTitle3, setRecipeTitle3] = useState("");
+
+    const[checkBoxLabel1, setCheckBoxLabel1] = useState("Picture 1");
+    const[isChecked1, setIsChecked1] = useState(false);
+    const[checkBoxLabel2, setCheckBoxLabel2] = useState("Picture 2");
+    const[isChecked2, setIsChecked2] = useState(false);
+    const[checkBoxLabel3, setCheckBoxLabel3] = useState("Picture 3");
+    const[isChecked3, setIsChecked3] = useState(false);
+
+    const checkHandler = () => {
+        setIsChecked1(!isChecked1)
+    }
+    const checkHandler2 = () => {setIsChecked2(!isChecked2)}
+    const checkHandler3 = () => {setIsChecked3(!isChecked3)}
 
     async function fetchIngredient(ingredientName){
         const myHeaders = new Headers();
@@ -39,17 +57,19 @@ export default function BonusGame({score, setScore}){
         };
 
         let urlString = "https://api.spoonacular.com/recipes/"+id+"/information?apiKey=49731ab381b24450981eafd9df6a69b0";
+        console.log(urlString);
         
         fetch(urlString, requestOptions)
             .then((response) => response.json())
             .then((result) => {
                 console.log(result.image);
                 setRecipeImage3(result.image);
+                // setRecipeTitle3(result.title);
             })
             .catch((error) => console.error(error));
     }
 
-    function fetchRandomRecipe(setFunction) {
+    function fetchRandomRecipe(setImageFunction, setTitleFunction) {
         const requestOptions = {
             method: "GET",
             redirect: "follow"
@@ -59,11 +79,19 @@ export default function BonusGame({score, setScore}){
             .then((response) => response.json())
             .then((result) => {
                 console.log(result.recipes[0]);
-                setFunction(result.recipes[0].image);
+                setImageFunction(result.recipes[0].image);
+                // setTitleFunction(result.recipes[0].title);
             })
             .catch((error) => console.error(error));
     }
-    
+
+    function checkAnswer(){
+        console.log("Test");
+        if(isChecked3){
+
+        }
+    }
+
     return(
         <>
             <br/>
@@ -84,18 +112,38 @@ export default function BonusGame({score, setScore}){
                 variant="outlined"
                 onClick={() => {
                     fetchIngredient(ingredient);
-                    fetchRandomRecipe(setRecipeImage1);
-                    fetchRandomRecipe(setRecipeImage2);
                     getImageByID(id);
+                    fetchRandomRecipe(setRecipeImage1, setRecipeTitle1);
+                    fetchRandomRecipe(setRecipeImage2, setRecipeTitle2);
                 }
                 }
             >
                 Submit
             </Button>
             <br/>
-            <img src={recipeImage1} height="100px"/>{'    '}
-            <img src={recipeImage2} height="100px"/>{'    '}
-            <img src={recipeImage3} height="100px"/>
+            <img src={recipeImage1} height={imageHeightValue}/>{'    '}
+            <img src={recipeImage2} height={imageHeightValue}/>{'    '}
+            <img src={recipeImage3} height={imageHeightValue}/>
+            <br/>
+            <div>
+                <input type="checkbox" id="checkbox1" checked={isChecked1} onChange={checkHandler}/>
+                <label htmlFor="checkbox1">{checkBoxLabel1}</label>
+                <br/>
+                <input type="checkbox" id="checkbox2" checked={isChecked2} onChange={checkHandler2}/>
+                <label htmlFor="checkbox2">{checkBoxLabel2}</label>
+                <br/>
+                <input type="checkbox" id="checkbox3" checked={isChecked3} onChange={checkHandler3}/>
+                <label htmlFor="checkbox3">{checkBoxLabel3}</label>
+            </div>
+            <Button 
+                variant="outlined"
+                onClick={() => {
+                    checkAnswer();
+                }
+                }
+            >
+                Check Answer
+            </Button>
         </>
     )
 
