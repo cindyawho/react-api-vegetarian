00f8d47f64cf46ceb1b51c6e9fd1999ddc842ab2
Cindy Andrade <cindya3939@gmail.com>
2024-07-03 12:14:30 -0700

    Update images when user clicks submit

--- src/BonusGame.js
+++ src/BonusGame.js
@@ -7,6 +7,10 @@ import Button from '@mui/material/Button';
 export default function BonusGame({score, setScore}){
     const[id, setID] = useState("");
     const[ingredient, setIngredient] = useState("");
+    const[recipeImage1, setRecipeImage1] = useState("https://images.unsplash.com/photo-1533745848184-3db07256e163?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+    const[recipeImage2, setRecipeImage2] = useState("https://images.unsplash.com/photo-1533745848184-3db07256e163?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+    const[recipeImage3, setRecipeImage3] = useState("https://images.unsplash.com/photo-1533745848184-3db07256e163?q=80&w=3132&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+
     async function fetchIngredient(ingredientName){
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
@@ -17,7 +21,7 @@ export default function BonusGame({score, setScore}){
         redirect: "follow"
         };
 
-        let urlString = "https://api.spoonacular.com/recipes/search?query="+"egg"+"&apiKey=49731ab381b24450981eafd9df6a69b0";
+        let urlString = "https://api.spoonacular.com/recipes/search?query="+ingredientName+"&apiKey=49731ab381b24450981eafd9df6a69b0";
 
         fetch(urlString, requestOptions)
         .then((response) => response.json())
@@ -28,6 +32,38 @@ export default function BonusGame({score, setScore}){
         .catch((error) => console.error(error));
     }
 
+    function getImageByID(id){
+        const requestOptions = {
+            method: "GET",
+            redirect: "follow"
+        };
+
+        let urlString = "https://api.spoonacular.com/recipes/"+id+"/information?apiKey=49731ab381b24450981eafd9df6a69b0";
+        
+        fetch(urlString, requestOptions)
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result.image);
+                setRecipeImage3(result.image);
+            })
+            .catch((error) => console.error(error));
+    }
+
+    function fetchRandomRecipe(setFunction) {
+        const requestOptions = {
+            method: "GET",
+            redirect: "follow"
+        };
+        
+        fetch("https://api.spoonacular.com/recipes/random?apiKey=49731ab381b24450981eafd9df6a69b0", requestOptions)
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result.recipes[0]);
+                setFunction(result.recipes[0].image);
+            })
+            .catch((error) => console.error(error));
+    }
+    
     return(
         <>
             <br/>
@@ -48,11 +84,18 @@ export default function BonusGame({score, setScore}){
                 variant="outlined"
                 onClick={() => {
                     fetchIngredient(ingredient);
+                    fetchRandomRecipe(setRecipeImage1);
+                    fetchRandomRecipe(setRecipeImage2);
+                    getImageByID(id);
                 }
                 }
             >
                 Submit
             </Button>
+            <br/>
+            <img src={recipeImage1} height="100px"/>{'    '}
+            <img src={recipeImage2} height="100px"/>{'    '}
+            <img src={recipeImage3} height="100px"/>
         </>
     )
 
